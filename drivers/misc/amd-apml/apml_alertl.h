757dfe4de6fa4a3400e6c971a351b36cc84b69fb
Sathya Priya Kumar <SathyaPriya.K@amd.com>
2025-11-19 22:21:25 +0530

    misc: amd-apml: Update Alert_L driver to use global device registry
    Modify device tree-based device discovery in Alert_L driver with
    centralized device registry from apml_common module. This provides
    more reliable to sbrmi/sbtsi device access for processing alerts
    and eliminates DTS parsing.
    
    Reviewed-by: Naveen Krishna Chatradhi <naveenkrishna.chatradhi@amd.com>
    Signed-off-by: Akshay Gupta <akshay.gupta@amd.com>
    Signed-off-by: sathya priya kumar <SathyaPriya.K@amd.com>

--- drivers/misc/amd-apml/apml_alertl.h
+++ drivers/misc/amd-apml/apml_alertl.h
@@ -6,22 +6,13 @@
 #ifndef _AMD_APML_ALERT_L__
 #define _AMD_APML_ALERT_L__
 
-#include "sbrmi-common.h"
-#include "sbtsi-common.h"
+#include "apml_common.h"
 
-/* APML_ALERTL Sends uevent to userspace for temparature
- * alert and  RAS (fatal and non-fatal) error, including
- * environmental data such as socket/die and alertl source
- * information.
- */
-
+/* struct apml_alertl_data - APML Alert_L driver data structure */
 struct apml_alertl_data {
-	struct apml_sbrmi_device **rmi_dev;
-	struct apml_sbtsi_device **tsi_dev;
 	struct device *dev;
-	atomic_t removed;
-	u8 num_of_rmi_devs;
-	u8 num_of_tsi_devs;
+	struct gpio_desc *alertl_gpiod;
+	int irq_num;
 } __packed;
 
 #endif /*_AMD_APML_ALERT_L__*/
