b79c4d3b321083d21d31bb1981c25ecca330045f
Sathya Priya Kumar <SathyaPriya.K@amd.com>
2025-11-19 22:18:44 +0530

    misc: amd-apml: Integrate SBRMI with common device registration framework
    Add device registration/unregistration calls to SBRMI drivers. This
    enables Alert_L driver to discover and safely access registered RMI
    devices for processing RAS event.
    
    Remove device tree matching functions from SBRMI drivers that are no
    longer needed after migrating Alert_L driver to use the global device
    registry from apml_common framework.
    
    The legacy Alert_L module used *_match_i2c/*_match_i3c functions to
    retrieve device node information from device tree nodes. With the new
    design, Alert_L module devices are now registered during each module's
    probe function, making the old matching functions obsolete.
    
    Reviewed-by: Naveen Krishna Chatradhi <naveenkrishna.chatradhi@amd.com>
    Signed-off-by: sathya priya kumar <SathyaPriya.K@amd.com>
    Signed-off-by: Akshay Gupta <akshay.gupta@amd.com>

--- drivers/misc/amd-apml/sbrmi.c
+++ drivers/misc/amd-apml/sbrmi.c
@@ -22,6 +22,7 @@
 #include <linux/version.h>
 
 #include "sbrmi-common.h"
+#include "apml_common.h"
 
 /* Do not allow setting negative power limit */
 #define SBRMI_PWR_MIN	0
@@ -498,6 +499,7 @@ static int sbrmi_i2c_probe(struct i2c_client *client)
 	struct device *hwmon_dev;
 	struct apml_sbrmi_device *rmi_dev;
 	const char *hwmon_dev_name;
+	int ret;
 
 	rmi_dev = devm_kzalloc(dev, sizeof(struct apml_sbrmi_device), GFP_KERNEL);
 	if (!rmi_dev)
@@ -524,7 +526,15 @@ static int sbrmi_i2c_probe(struct i2c_client *client)
 		return PTR_ERR_OR_ZERO(hwmon_dev);
 
 	init_completion(&rmi_dev->misc_fops_done);
-	return create_misc_rmi_device(rmi_dev, dev);
+	ret = create_misc_rmi_device(rmi_dev, dev);
+	if (ret)
+		return ret;
+	/* Register with ALERT_L common system */
+	ret = apml_register_sbrmi_device(rmi_dev);
+	if (ret)
+		dev_warn(dev, "Failed to register with ALERT_L common system: %d\n", ret);
+
+	return ret;
 }
 
 static int sbrmi_i3c_reg_read(struct i3c_device *i3cdev, int reg_size, u32 *val)
@@ -720,7 +730,15 @@ static int sbrmi_i3c_probe(struct i3c_device *i3cdev)
 	}
 
 	init_completion(&rmi_dev->misc_fops_done);
-	return create_misc_rmi_device(rmi_dev, dev);
+	ret = create_misc_rmi_device(rmi_dev, dev);
+	if (ret)
+		return ret;
+	/* Register with ALERT_L common system */
+	ret = apml_register_sbrmi_device(rmi_dev);
+	if (ret)
+		dev_warn(dev, "Failed to register with ALERT_L common system: %d\n", ret);
+
+	return ret;
 }
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(6, 1, 0)
@@ -737,7 +755,8 @@ static void sbrmi_i2c_remove(struct i2c_client *client)
 #else
 		return;
 #endif
-
+	/* Unregister from APML common system */
+	apml_unregister_sbrmi_device(rmi_dev);
 	/*
 	 * Set the no_new_trans so no new transaction can
 	 * occur in sbrmi_ioctl
@@ -777,7 +796,8 @@ static void sbrmi_i3c_remove(struct i3c_device *i3cdev)
 #else
 		return;
 #endif
-
+	/* Unregister from APML common system */
+	apml_unregister_sbrmi_device(rmi_dev);
 	/*
 	 * Set the no_new_trans so no new transaction can
 	 * occur in sbrmi_ioctl
@@ -855,26 +875,6 @@ static struct i3c_driver sbrmi_i3c_driver = {
 };
 
 module_i3c_i2c_driver(sbrmi_i3c_driver, &sbrmi_driver)
-
-int sbrmi_match_i3c(struct device *dev, const void *data)
-{
-	const struct device_node *node = (const struct device_node *)data;
-
-	if (dev->of_node == node && dev->driver == &sbrmi_i3c_driver.driver)
-		return 1;
-	return 0;
-}
-EXPORT_SYMBOL_GPL(sbrmi_match_i3c);
-
-int sbrmi_match_i2c(struct device *dev, const void *data)
-{
-	const struct device_node *node = (const struct device_node *)data;
-
-	if (dev->of_node == node && dev->driver == &sbrmi_driver.driver)
-		return 1;
-	return 0;
-}
-EXPORT_SYMBOL_GPL(sbrmi_match_i2c);
 
 MODULE_AUTHOR("Akshay Gupta <akshay.gupta@amd.com>");
 MODULE_AUTHOR("Naveenkrishna Chatradhi <naveenkrishna.chatradhi@amd.com>");
